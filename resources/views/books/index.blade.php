4920a36a5947b101ae0b4e8933454f65ff6d7d24
Masaki-Ishii963 <masaki.ishii@luxiar.com>
2023-05-28 18:41:32 +0900

    登録フォームの作成

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -11,6 +11,19 @@
     <h1>BibLog</h1>
     <h2>読書の記録ができます。</h2>
     <div>
+        <p>登録フォーム</p>
+        <form action="{{ route('book.create') }}" method="post">
+            @csrf
+            <label for="book-title">タイトル</label>
+            <span>255文字まで</span>
+            <textarea id="book-title" type="text" name="book" placeholder="タイトルを入力"></textarea>
+            @error('book')
+            <p style="color: red;">{{ $message }}</p>
+            @enderror
+            <button type="submit">登録</button>
+        </form>
+    </div>
+    <div>
         @foreach ($books as $book)
             <p>{{ $book->title }}</p>
         @endforeach
