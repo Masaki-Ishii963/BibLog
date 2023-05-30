157ded59c85880a5dc95bb4e0d33e601a354905f
Masaki-Ishii963 <masaki.ishii@luxiar.com>
2023-05-30 19:53:10 +0900

    Laravel Breezeのインストールやり直し

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Book
 Route::get('/book', \App\Http\Controllers\Book\IndexController::class)
     ->name('book.index');
 Route::post('/create', \App\Http\Controllers\Book\CreateController::class)
@@ -27,3 +29,16 @@ Route::put('/update/{id}', \App\Http\Controllers\Book\Update\PutController::clas
     ->name('book.update.put');
 Route::delete('/delete/{id}', \App\Http\Controllers\Book\DeleteController::class)
     ->name('book.delete');
+
+// Laravel Breezeで自動生成されたルーティング
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
